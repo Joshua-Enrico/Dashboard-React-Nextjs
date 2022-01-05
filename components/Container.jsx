a3c0565bee6352ed1e7c5c5031c283625659a346
yoyogold-a11 <tmrfack@gmail.com>
2022-01-04 21:47:30 -0500

    first commit

---
+++ components/Container.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Cont = styled.div`
+
+`;
+
+
+const Container = () => {
+    return (
+        <Cont>
+            
+        </Cont>
+    )
+}
+
+export default Container
